c1d74a573fa2516a6c746f7e13b1537c9619dcf9
unknown <zlatovvd@ukr.net>
2024-05-01 16:52:23 +0300

    0105w

---
+++ src/app/layout.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './globals.css';
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body>{children}</body>
+    </html>
+  );
+}
